0f6914c67ce6cbbe5237a27484a023922aa69f85
root <alexbzg@gmail.com>
2020-06-04 13:41:25 +0300

    upload_srv sigterm exits gracefully

--- oneadif/upload_srv.py
+++ oneadif/upload_srv.py
@@ -1,18 +1,25 @@
 #!/usr/bin/python3
 #coding=utf-8
 
-from multiprocessing.connection import Listener
 import multiprocessing
+from multiprocessing.connection import Listener
 import threading
 import logging
 import time
 import uuid
+import socket
+import signal
+import sys
 from asyncio import CancelledError
 
 from db import DBConn, splice_params
 from conf import CONF, start_logging
 from elog import ELog
 
+UPLOAD_PROCESSES = {}
+DB = DBConn(CONF.items('db'))
+DB.connect()
+DB.verbose = True
 
 class PipeListener(threading.Thread):
 
@@ -37,7 +44,9 @@ class UploadConnector(PipeListener):
         super().__init__(self, self.on_pipe_data)
 
     def on_pipe_data(self, state):
-        UPLOAD_PROCESSES[self.upload_id]['state'] = state
+        up_record = UPLOAD_PROCESSES[self.upload_id]
+        up_record['state'] = state
+        up_record['time'] = time.time()
         if state in ['login failed', 'upload failed', 'success', 'cancelled']:
             self.__upload_process.join()
 
@@ -116,7 +125,8 @@ class UploadProcess(multiprocessing.Process):
             if elog.login(self.login_data):
                 self.__raise_for_cancel()
                 self.state = 'upload'
-                if elog.upload(self.file, self.params, callback=self.upload_callback, cancel_event=self.__cancel_event):
+                if elog.upload(self.file, self.params, callback=self.upload_callback,\
+                    cancel_event=self.__cancel_event):
                     self.state = 'success'
                 else:
                     self.state = 'upload failed'
@@ -130,41 +140,60 @@ class UploadProcess(multiprocessing.Process):
         finally:
             self.__pipe.send(self.state)
 
-def listen():
-    listener_address = (CONF['files']['uploads_srv_socket'], 'AF_UNIX')
-    with Listener(listener_address) as listener:
-        while True:
-            with listener.accept() as conn:
-                data = conn.recv()
-                if data[0] == 'upload':
-                    pipe_process, pipe_connector = multiprocessing.Pipe()
-                    upload_process = UploadProcess(pipe_process, *data[1])
-                    upload_connector = UploadConnector(pipe_connector, upload_process)
-                    UPLOAD_PROCESSES[upload_process.upload_id] = {\
-                        'connector': upload_connector,\
-                        'state': 'init'}
-                    upload_process.start()
-                    upload_connector.start()
-                    conn.send(upload_process.upload_id)
-                elif data[0] == 'cancel':
-                    upload_id = data[0]
-                    error = None
-                    if upload_id in UPLOAD_PROCESSES:
-                        upload = UPLOAD_PROCESSES[upload_id]
-                        if upload['state'] in ['init', 'login', 'upload']:
-                            upload['connector'].send('cancel')
-                        else:
-                            error = 'Upload cannot be cancelled'
-                    else:
-                        error = 'Upload not found'
-                    conn.send(error if error else 'ok')
+def conn_worker(conn, _up=UPLOAD_PROCESSES):
+    logging.debug('Connection received')
+    data = conn.recv()
+    logging.debug('Received data:')
+    logging.debug(data)
+    if data[0] == 'upload':
+        pipe_process, pipe_connector = multiprocessing.Pipe()
+        upload_process = UploadProcess(pipe_process, *data[1])
+        upload_connector = UploadConnector(pipe_connector, upload_process)
+        _up[upload_process.upload_id] = {\
+            'connector': upload_connector,\
+            'state': 'init'}
+        upload_process.start()
+        upload_connector.start()
+        conn.send(upload_process.upload_id)
+    elif data[0] == 'cancel':
+        upload_id = data[0]
+        error = None
+        if upload_id in _up:
+            upload = _up[upload_id]
+            if upload['state'] in ['init', 'login', 'upload']:
+                upload['connector'].send('cancel')
+            else:
+                error = 'Upload cannot be cancelled'
+        else:
+            error = 'Upload not found'
+        conn.send(error if error else 'ok')
+    elif data[0] == 'test':
+        conn.send(data)
+    conn.close()
+
+def __sigterm(signum, frame):
+    logging.debug('Term signal')
+    sys.exit(0)
+
+def serve_forever(address, family):
+    try:
+        logging.info('Starting server')
+        signal.signal(signal.SIGTERM, __sigterm)
+        with Listener(address, family) as listener:
+            listener._listener._socket.settimeout(0.1)
+            listener._listener._socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            while True:
+                try:
+                    conn = listener.accept()
+                    conn_thread = threading.Thread(target=conn_worker, args=(conn,))
+                    conn_thread.daemon = True
+                    conn_thread.start()
+                except socket.timeout:
+                    continue
+    except Exception as exc:
+        logging.exception('Stopping server')
+        raise exc
 
 if __name__ == "__main__":
     start_logging('upload_srv', CONF['logs']['upload_srv_level'])
-
-    UPLOAD_PROCESSES = {}
-    DB = DBConn(CONF.items('db'))
-    DB.connect()
-    DB.verbose = True
-
-    listen()
+    serve_forever(CONF['files']['upload_server_socket'], 'AF_UNIX')
